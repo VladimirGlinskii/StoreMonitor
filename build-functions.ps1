112350eda65abbd45b524117b233ff905c88459a
Vladimir Glinskii <vladimir.glinskiy99@gmail.com>
2024-04-11 17:41:13 +0600

    build: use 7z instead tar

--- build-functions.ps1
+++ build-functions.ps1
@@ -4,5 +4,9 @@ mvn -f ./functions-parent/pom.xml install
 $functionNames = 'auth-function', 'cashier-simulator', 'update-sensor-value', 'sensor-simulator', 'create-incident', `
   'incidents-report-function'
 foreach ($functionName in $functionNames) {
-  tar -a -c -f "functions-build\$functionName.zip" -C "functions-parent\$functionName\target\code" *
+  $targetZipName = "functions-build\$functionName.zip"
+  if (Test-Path $targetZipName) {
+    Remove-Item $targetZipName
+  }
+  7z u $targetZipName ".\functions-parent\$functionName\target\code\*"
 }
